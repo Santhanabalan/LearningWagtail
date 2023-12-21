3c95243e30ae31161ff0b446dfb00b521ae557f1
Santhanabalan <bala2102002@gmail.com>
2023-12-21 13:16:01 +0530

    Wagtail: BUGIFX Images not rendered
    get_context() method and tags are not configured properly

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from wagtail.models import Page
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.search import index
+from modelcluster.fields import ParentalKey
 
 # Create your models here.
 
@@ -12,8 +13,8 @@ class BlogIndexPage(Page):
 
     content_panels = Page.content_panels + [FieldPanel("intro")]
 
-    def get_context(self):
-        context = super().get_context()
+    def get_context(self, request):
+        context = super().get_context(request)
         blogpages = self.get_children().live().order_by("-first_published_at")
         context["blogpages"] = blogpages
         return context
@@ -24,6 +25,13 @@ class BlogPage(Page):
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
 
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
     search_fields = Page.search_fields + [
         index.SearchField("intro"),
         index.SearchField("body"),
@@ -33,4 +41,20 @@ class BlogPage(Page):
         FieldPanel("date"),
         FieldPanel("intro"),
         FieldPanel("body"),
+        InlinePanel("gallery_images", label="Gallery images"),
+    ]
+
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(
+        BlogPage, on_delete=models.CASCADE, related_name="gallery_images"
+    )
+    image = models.ForeignKey(
+        "wagtailimages.Image", on_delete=models.CASCADE, related_name="+"
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel("image"),
+        FieldPanel("caption"),
     ]
