53e59318a89fc7fee98d1cd2eab826198f40bac2
Santhanabalan <bala2102002@gmail.com>
2023-12-21 14:00:03 +0530

    Wagtail: Add Tags to blogs and list blogs based on tags

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,14 @@
 from django import forms
 from django.db import models
+
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
 from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.search import index
-from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from wagtail.snippets.models import register_snippet
 
 # Create your models here.
@@ -27,6 +31,7 @@ class BlogPage(Page):
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
     authors = ParentalManyToManyField("blog.Author", blank=True)
+    tags = ClusterTaggableManager(through="BlogPageTag", blank=True)
 
     def main_image(self):
         gallery_item = self.gallery_images.first()
@@ -45,6 +50,7 @@ class BlogPage(Page):
             [
                 FieldPanel("date"),
                 FieldPanel("authors", widget=forms.CheckboxSelectMultiple),
+                FieldPanel("tags"),
             ],
             heading="Blog information",
         ),
@@ -67,6 +73,22 @@ class BlogPageGalleryImage(Orderable):
         FieldPanel("image"),
         FieldPanel("caption"),
     ]
+
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        "BlogPage", related_name="tagged_items", on_delete=models.CASCADE
+    )
+
+
+class BlogTagIndexPage(Page):
+    def get_context(self, request):
+        tag = request.GET.get("tag")
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        context = super().get_context(request)
+        context["blogpages"] = blogpages
+        return context
 
 
 @register_snippet
